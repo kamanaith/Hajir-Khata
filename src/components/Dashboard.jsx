d76c950f1673710447eea24ee7836da1707415f9
kamanaith <timsinakamaksha@gmail.com>
2025-03-28 08:23:55 +0545

    Dashboard updated

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
-import Dashboard1 from "../assets/images/Dashboard/Dashboard1.png";
-import Dashboard2 from "../assets/images/Dashboard/Dashboard2.png";
-import Admin1 from "../assets/images/Dashboard/Admin1.png";
-import User1 from "../assets/images/Dashboard/User1.png";
+
 
 const tabs = ["Dashboard", "Admin App", "User App"];
 
@@ -28,32 +25,32 @@ function Dashboard() {
           ))}
         </div>
 
-        {/* Image Display */}
+      
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {activeTab === "Dashboard" && (
             <div className="flex flex-col gap-4 md:flex md:flex-row ">
               <img
-                src={Dashboard1}
+                src="/Dashboard/Dashboard1.png"
                 alt="Dashboard View"
                 className="w-full rounded-lg p-10 shadow-lg"
               />
               <img
-                src={Dashboard2}
+                src="/Dashboard/Dashboard2.png"
                 alt="Dashboard View"
-                className="w-full rounded- p-10 shadow-lg"
+                className="w-full rounded-lg p-10 shadow-lg"
               />
             </div>
           )}
           {activeTab === "Admin App" && (
             <img
-              src={Admin1}
+              src="/Dashboard/Admin1.png"
               alt="Admin App View"
               className="w-fit rounded-lg p-10 shadow-lg"
             />
           )}
           {activeTab === "User App" && (
             <img
-              src={User1}
+              src="/Dashboard/User1.png"
               alt="User App View"
               className="w-full rounded-lg p-10 shadow-lg"
             />
